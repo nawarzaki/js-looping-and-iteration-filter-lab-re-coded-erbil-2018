aa127823dfb69d282d8ef5854575a333fba9fc30
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-09 18:55:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,17 +19,9 @@ function fuzzyMatch(drivers, letters) {
 function matchName(drivers, name) {
 let x = drivers.filter(function(nam){return Object.keys(nam).some(function(key) {
   return nam[key].includes(name)
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 95815445487260d1454d1b3d49f3bdfb39eb11e9
 })
 })
 return x
 
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 95815445487260d1454d1b3d49f3bdfb39eb11e9
