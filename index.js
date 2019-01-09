7052fa5b00ecfcb7352f3ae1f58e17332e748fc6
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-09 18:47:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file
+
+
+function findMatching(drivers, name) {
+
+  let x = drivers.filter(function(nam){return nam.toUpperCase()===name.toUpperCase()})
+  return x
+}
+
+function fuzzyMatch(drivers, letters) {
+  let x
+  if (letters[0]==="S"){
+    x = drivers.filter(function(nam){return nam[0]==="S"})
+  }else if (letters[0] != "S"){
+    x = []
+  }
+  return x
+}
+
+function matchName(drivers, name) {
+let x = drivers.filter(function(nam){return Object.keys(nam).some(function(key) {
+  return nam[key].includes(name)
+
+})
+})
+return x
+
+}
